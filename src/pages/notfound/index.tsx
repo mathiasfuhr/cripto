1b161f94415c24c65d420cd326b51b68cbec24d5
mathiasfuhr <mathiasgilvanf@gmail.com>
2024-06-15 14:00:23 -0300

    first commit

---
+++ src/pages/notfound/index.tsx
@@ -0,0 +1,7 @@
+export function Notfound() {
+  return (
+    <div>
+      <h1>Pagina 404 não existe</h1>
+    </div>
+  );
+}
---
+++ src/routes.tsx
@@ -0,0 +1,20 @@
+import { createBrowserRouter } from "react-router-dom";
+
+import { Home } from "./pages/home";
+import { Detail } from "./pages/detail";
+import { Notfound } from "./pages/notfound";
+import { Layout } from "./components/layout";
+
+const router = createBrowserRouter([
+  {
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
+  },
+]);
+
+export { router };
