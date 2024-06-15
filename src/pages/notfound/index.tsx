778cad9e03f89531ed881d4524ea5ced88f7ffeb
mathiasfuhr <mathiasgilvanf@gmail.com>
2024-06-15 14:16:24 -0300

    primeiro commit

--- src/pages/notfound/index.tsx
+++ src/pages/notfound/index.tsx
@@ -1,7 +1,13 @@
-export function Notfound() {
-  return (
-    <div>
-      <h1>Pagina 404 não existe</h1>
+import styles from './notfound.module.css'
+import { Link } from 'react-router-dom'
+
+export function Notfound(){
+  return(
+    <div className={styles.container}>
+      <h1>Pagina 404 nao existe</h1>
+      <Link to="/">
+        Acessar cripto moedas
+      </Link>
     </div>
-  );
+  )
 }
